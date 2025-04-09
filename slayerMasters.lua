85c15c79328e219f766cb82c81b528f54760ae9e
0xB0BABABE <0xbobababe@proton.me>
2025-04-09 02:36:36 +0100

    renamed main -> slayer renamed tasks -> slayerTasks added Laniakea added getNewTask added skipTask

--- slayerMasters.lua
+++ slayerMasters.lua
@@ -1,17 +1,19 @@
 ---@class SlayerMaster
 ---@field id number
 ---@field name string
----@field levelRequirement number
+---@field slayerLevelRequirement number
+---@field combatLevelRequirement number
 ---@field location WPOINT
 ---@field otherConditions function
 
----@type table<number, SlayerMaster>
+---@type table<string, SlayerMaster>
 local SlayerMasters = {
-    [1] = {
-        id = 1,
-        name = "",
-        levelRequirement = 1,
-        location = WPOINT.new(0, 0, 0),
+    ["Laniakea"] = {
+        id = 26558,
+        name = "Laniakea",
+        slayerLevelRequirement = 90,
+        combatLevelRequirement = 120,
+        location = WPOINT.new(5668, 2138, 0),
         otherConditions = function() return true end
     },
 }
